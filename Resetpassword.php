99e58ba7c167c4de2581eba42d419a6f8ef530ad
Afsha-na <88855556+Afsha-na@users.noreply.github.com>
2022-08-07 07:50:40 -0400

    Update Resetpassword.php

--- Resetpassword.php
+++ Resetpassword.php
@@ -1,38 +1,27 @@
-<?php
+<?php require_once 'db_con.php'; 
 session_start(); 
-include('myhead.php');
 
 if(isset($_POST['Resetpassword']))
 {
     $email=$_POST['email'];
-mysqli_real_escape_string($con,$email);
-$myemail="SELECT * FROM `add_admin` WHERE aemail='$email' LIMIT 1";
-$query=mysqli_query($con,$myemail);
+mysqli_real_escape_string($db_con,$email);
+$myemail="SELECT * FROM `users` WHERE email='$email' LIMIT 1";
+$query=mysqli_query($db_con,$myemail);
 if(mysqli_num_rows($query) > 0)
 {
 while($row = mysqli_fetch_array($query)){
-//echo $row;
-$get_pass=$row['apassword'];
-echo $get_pass;
+$get_pass=$row['password'];
+echo "<h1 align='center' style='color:red'>your password is =  $get_pass <h1>";
+ echo "<h1 align='center'><button><a class='btn btn-danger' href='login.php'><span style='color:red;'>Login</span></a></button></h1>";
 }
-// if($get_pass)
-// {
-// send_password_reset($get_pass,$email);
-//     $_SESSION['status']="We emailed you a password reset link";
-//     header('location:Resetpassword');
-//     exit(0);
-// }
-// else
-// {
-//     die(mysqli_error($con));
-//     $_SESSION['status']="Something went wrong";
-//     header('location:Resetpassword');
-//     exit(0);  
-// }
 }
 else
-{
-    die(mysqli_error($con));
+{?>
+	<div>
+	<p  ><h3 align="center" style="color:red">No email found</h3></p></div>
+  <?php 
+   echo "<h3 align='center'><button><a class='btn btn-danger' href='forgotpassword.php'><span style='color:red;'>Back</span></a></button></h3>";
+  die(mysqli_error($db_con));
     $_SESSION['status']="No Email Found";
     header('location:Resetpassword');
     exit(0);
